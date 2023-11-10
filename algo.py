c363c981963258a6a9cf80390c306f0f46490878
Cantor Alexandru <alexcantor64@gmail.com>
2023-11-10 12:27:12 +0200

    Add entropy hte function calculation

--- algo.py
+++ algo.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 # Create a partition based on consecutive dates
-def create_partition(data, num_partitions=500):
-    sorted_data = data.sort_values(by='Date')
-
+def create_finite_partition(data, num_partitions):
     points_per_partition = len(data) // num_partitions
 
     consecutive_partitions = []
@@ -11,14 +9,15 @@ def create_partition(data, num_partitions=500):
     for i in range(num_partitions):
         start_index = i * points_per_partition
         end_index = (i + 1) * points_per_partition
-        partition = sorted_data.iloc[start_index:end_index]
+        partition = data.iloc[start_index:end_index]
         consecutive_partitions.append(partition)
 
     return consecutive_partitions
 
 
 stock_data = pd.read_csv('spx.csv')
-closing_price_partitions = create_partition(stock_data, 5000)
+sorted_data = stock_data.sort_values(by='Date')
+closing_price_partitions = create_finite_partition(sorted_data, 5000)
 
 
 
---
+++ entropy_hTe.py
@@ -0,0 +1,25 @@
+import itertools
+import math
+
+# Calculates the limit entropy by iterating over all possible combinations of indices representing subsets in the partition. 
+# It computes the intersection probabilities of the sequence with these subsets, multiplies them, and accumulates in sum_entropy.
+def calculate_limit_entropy(sequence, partition):
+    n = len(partition)
+    sum_entropy = 0
+
+    for combination in itertools.product(range(n), repeat=n):
+        print(combination)
+        intersection_prob = 1
+        for subsetIndex in range(n):
+            subset = partition[combination[subsetIndex]]
+            intersection_prob *= len(set(sequence).intersection(set(subset))) / n
+
+        sum_entropy += intersection_prob * math.log(intersection_prob)
+
+    return -sum_entropy / n
+
+# Example usage
+sequence = [1, 2, 3, 4, 5]
+finite_partition = [[1, 2], [3, 4, 5]]  # Example finite partition
+limit_entropy = calculate_limit_entropy(sequence, finite_partition)
+print(f"The limit entropy for the sequence {sequence} with the given partition is: {limit_entropy}")
